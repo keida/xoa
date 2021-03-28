acd3323ff60937a4c6ad7a452edbc53150973812
keida <woohyuk0909@sina.com>
2021-03-28 22:30:04 +1300

    change

--- src/store/actions/check.js
+++ src/store/actions/check.js
@@ -1,37 +1,9 @@
 import * as actionTypes from './actionTypes';
 
-import { mock } from '../utils/tool';
-class Actions {
-  static begin() {
-    return {
-      type: actionTypes.CHECK_PENDING,
-    };
-  }
-  static ok(data, cb) {
-    cb && 'function' === typeof cb && cb(data);
-    return {
-      type: actionTypes.CHECK_SUCCESS,
-      payload: data,
-    };
-  }
-  static fail(data, cb) {
-    cb && 'function' === typeof cb && cb(data);
-    return {
-      type: actionTypes.CHECK_FAILURE,
-      payload: data,
-    };
-  }
-}
-
-const actionCreator = (data, cb) => {
-  return (dispatch, getState) => {
-    dispatch(Actions.begin());
-    mock()
-      .then(() => {
-        dispatch(Actions.ok(data, cb));
-      })
-      .catch(error => dispatch(Actions.fail(error || 'Create failed', cb)));
+const actions = data => {
+  return {
+    type: actionTypes.CHECK_FAILURE,
+    payload: data,
   };
 };
-export default actionCreator;
-
+export default actions;
--- src/store/reducers/checkReducer.js
+++ src/store/reducers/checkReducer.js
@@ -2,17 +2,11 @@ import * as actionTypes from '../actions/actionTypes';
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case actionTypes.EDIT_PENDING:
-      return Object.assign({}, state);
-    case actionTypes.CHECK_SUCCESS:
+    case actionTypes.CHECK_FAILURE:
       let curDone = state.remindDos[action.payload];
       state.remindDos.splice(action.payload, 1);
       return Object.assign({}, state, {
         failDos: [...state.failDos, curDone],
-      });
-    case actionTypes.CHECK_FAILURE:
-      return Object.assign({}, state, {
-        err: action.payload,
       });
     default:
       return state;
